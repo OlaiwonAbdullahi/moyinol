e2d46987f42ff94c617705d77d3956c555628b53
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2025-05-23 21:27:21 +0100

    refactor: remove unused Cta component import from Page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Hero from "./components/hero";
 import About from "./components/about";
 import Service from "./components/service";
 import Industries from "./components/industries";
-import Cta from "./ui/cta";
 
 const Page = () => {
   return (
