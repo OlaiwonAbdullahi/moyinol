8236d92006ab17ac9f62b6b30ac0ee68d5e7c192
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2025-05-25 15:23:41 +0100

    feat: add Values component to About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./components/hero";
 import Vision from "./components/vision";
 import Mission from "./components/mision";
+import Values from "./components/values";
 
 const Page = () => {
   return (
@@ -10,6 +11,7 @@ const Page = () => {
 
       <Vision />
       <Mission />
+      <Values />
     </div>
   );
 };
